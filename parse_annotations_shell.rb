08a29098a96fbd4704510f0ef841cbc59bd302dc
Jordan Smith <jblsmith@mediadhcp01.a02.aist.go.jp>
2015-03-07 00:28:00 +0900

    Minor cosmetic update to parse_annotations_shell.

--- parse_annotations_shell.rb
+++ parse_annotations_shell.rb
@@ -1,6 +1,5 @@
-require 'find'
- 
-Find.find("/Users/blahblahblah/SALAMI_data_v1.2/data") do |f|
+files = Dir.glob("/Users/blahblahblah/salami-data-private/annotations/*/*.txt") 
+files.each do |f|
    if f.split(".")[-1]=="txt" then
        puts f
        system("ruby get_sections_sept_2012.rb #{f}")
